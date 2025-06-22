1d88b42df5b8cea876c6080990f364b1ad080a3e
Frederick Elledge <frederickjye@gmail.com>
2025-06-22 17:18:04 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+
+morse_code_dictionary = {
+    "A": ".-",    "B": "-...",  "C": "-.-.",  "D": "-..",
+    "E": ".",     "F": "..-.",  "G": "--.",   "H": "....",
+    "I": "..",    "J": ".---",  "K": "-.-",   "L": ".-..",
+    "M": "--",    "N": "-.",    "O": "---",   "P": ".--.",
+    "Q": "--.-",  "R": ".-.",   "S": "...",   "T": "-",
+    "U": "..-",   "V": "...-",  "W": ".--",   "X": "-..-",
+    "Y": "-.--",  "Z": "--..",
+
+    "0": "-----", "1": ".----", "2": "..---", "3": "...--",
+    "4": "....-", "5": ".....", "6": "-....", "7": "--...",
+    "8": "---..", "9": "----.",
+
+    ".": ".-.-.-",",": "--..--","?": "..--..","'": ".----.",
+    "!": "-.-.--","/": "-..-.","(": "-.--.-","&": ".-...",
+    ":": "---...",";": "-.-.-.","=": "-...-","+": ".-.-.",
+    "-": "-....-","_": "..--.-","\"": ".-..-.","$": "...-..-",
+    "@": ".--.-."
+}
+
+user_input =(input("enter the String to translate you would like to be encripted").upper())
+
+if user_input.startswith(".") or user_input.startswith("-"):
+    words = user_input.strip().split(" ")
+    response = []
+    for word in words:
+        if word == "/":
+            response.append(" ")
+        else:
+            for key, value in morse_code_dictionary.items():
+                if value == word:
+                    response.append(key)
+                    break
+    print("".join(response))
+
+else:
+    words = list(user_input.strip().strip("/"))
+    response = []
+    for word in words:
+        if word == " ":
+            response.append("/")
+        else:
+            response.append(morse_code_dictionary[word])
+    print(" ".join(response))
+
+
